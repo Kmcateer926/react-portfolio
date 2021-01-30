9b858cfbd8b59426f5568c6d18379fc069018375
Kevin McAteer <kmcateer926@gmail.com>
2021-01-30 15:18:25 -0500

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "./containers/Home/Home";
+import Contact from "./containers/Contact/Contact";
+import Portfolio from "./containers/Portfolio/Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router basename="/">
+      <Route exact path="/" Component={Home} />
+      <Route exact path="/Contact" Component={Contact} />
+      <Route exact path="/Portfolio" Component={Portfolio} />
+    </Router>
   );
 }
 
