f46432c3da8006c5d04cfa8f74c3372df785c296
Kevin McAteer <kmcateer926@gmail.com>
2021-01-30 20:21:05 -0500

    working on contact page

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -2,9 +2,54 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            <h1>This is my Contact</h1>
+        <main className="row" id="contact">
+        <div className="col-sm-12">
+          <div className="col-sm-12">
+            <h1>Contact</h1>
+            <div>
+              <div className="contact">
+                <h4>Name</h4>
+                <p>
+                  Kevin Joseph McAteer
+                </p>
+              </div>
+              <div>
+                <h4>Email</h4>
+               <p>
+                 kmcateer926@gmail.com
+               </p>
+              </div>
+              <div>
+                <h4>Phone</h4>
+                <p>706-867-5611</p>
+              </div>
+
+              <div>
+                <h4>Github</h4>
+                <a
+                  href="https://github.com/Kmcateer926" rel="noreferrer noopener" target="_blank">github.com/Kmcateer926
+              </a>
+              </div>
+              <div>
+                <h4>Linkedin</h4>
+                <a
+                  href="https://www.linkedin.com/in/kevin-mcateer-27950b1b5/" rel="noreferrer noopener" target="_blank">linkedin.com/in/kevin-mcateer-27950b1b5
+              </a>
+              </div>
+              <div>
+                <h4>Resume</h4>
+                {/* <a href="assets\images\Resume (2020).pdf" target="_blank" download>
+                  <img src="assets\images\2020-10-28 (3).png" 
+                  alt="resume"
+                  style="width: 50; height:50px"
+                   >
+                </a> */}
+              </div>
+            </div>
+          </div>
         </div>
+      
+    </main>
     );
 };
 
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
-
+import React from "react";
+import "./Home.css"
+import image1 from "./image1.jpg";
+// var imageKevin = require(`./images/image1.jpg`)
 const Home = () => {
-    return (
-        <div>
-            <h1>This is my Home</h1>
+  return (
+    <main className="row">
+      <div className="col-sm-12">
+        <div className="col-sm-12">
+          <h1>About Me</h1>
+          <img
+            src={image1}
+            // className="img-fluid float-left"
+            alt="Kevin McAteer"
+          />
+
+          <p className="text-justify">
+            I grew up in a military family and lived all over the place, as a
+            result I love to travel. I attended the university of North Georgia
+            where I received a bachelors with a major in Arabic and minor in
+            international affairs. While in school, I was in the Georgia Army
+            National Guard for 8 years while working part time. While in the
+            army, I learned how to be a leader and to lead by example. The army
+            also taught me how to challenge myself, which is very rewarding.
+            After graduating and receiving a degree, I became a full-time
+            employee at NorthEast Sales distributing, working in multiple
+            territories such as Snellville, Canton, Athens, and Lawrenceville.
+            During my free time, I enjoy hiking, lifting weights, cooking,
+            fishing, etc. Traveling is a big deal for me as well, my most recent
+            adventure was in Alaska, where I was able to see a lot of wild
+            animals. Moose are a lot scarier than I expected them to be. I am
+            currently a student at GA tech coding bootcamp while working
+            full-time. Coding is a little difficult but I am learning a lot and
+            I am very proud of the progress that I have made. Last time I
+            updated this portfolio, I said that coding is difficult. It still
+            is, but I think I have come a long way. I now know how to set up
+            data bases with my own server and deploy it to sites like heroku. I
+            can also say I understand CRUD with my last project. I still have a
+            lot of work to do but I feel way more confident now, than I did in
+            October 2020.
+          </p>
         </div>
-    );
+      </div>
+    </main>
+  );
 };
 
 export default Home;
