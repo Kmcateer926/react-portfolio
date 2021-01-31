1f615992268d2752676398dca1747778fd8361bc
Kevin McAteer <kmcateer926@gmail.com>
2021-01-30 19:06:07 -0500

    added navbar that is fuctional

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import Contact from "./containers/Contact/Contact";
 import Portfolio from "./containers/Portfolio/Portfolio";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <Router basename="/">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/Contact" component={Contact} />
-      <Route exact path="/Portfolio" component={Portfolio} />
-    </Router>
+    <>
+      <Router basename="/">
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Contact" component={Contact} />
+        <Route exact path="/Portfolio" component={Portfolio} />
+      </Router>
+    </>
   );
 }
 
